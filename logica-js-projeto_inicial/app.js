22b86ffd44404e0eb5e87b39f72d122f9f4b8e93
sarah-campos <sarahstephanycampos@gmail.com>
2024-06-21 15:36:04 -0300

    condicionais e concatenacao

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,8 +1,12 @@
 alert('Boas-vindas ao jogo do número secreto!');
 
 let numeroSecreto = 29;
+console.log(numeroSecreto);
 let chute = prompt('Escolha um número entre 1 e 30');
 
 if(numeroSecreto == chute) {
-  console.log('Acertou!!!')
+  alert(`Você acertou! O número secreto era ${numeroSecreto}!`)
+}
+else {
+  alert(`Você errou :( O número secreto era ${numeroSecreto}.`);  
 }
