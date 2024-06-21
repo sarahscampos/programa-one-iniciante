32dc8147951026afd4ca253a49dd4fe260356406
sarah-campos <sarahstephanycampos@gmail.com>
2024-06-21 16:07:31 -0300

    loops e tentativas

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -2,11 +2,23 @@ alert('Boas-vindas ao jogo do número secreto!');
 
 let numeroSecreto = 29;
 console.log(numeroSecreto);
-let chute = prompt('Escolha um número entre 1 e 30');
+let chute;
+let tentativas = 1;
 
-if(numeroSecreto == chute) {
-  alert(`Você acertou! O número secreto era ${numeroSecreto}!`)
+while(chute != numeroSecreto) {
+  chute = prompt('Escolha um número entre 1 e 30');
+
+  if(numeroSecreto == chute) {
+    alert(`Você acertou! O número secreto era ${numeroSecreto}! Você precisou de ${tentativas} tentativas.`)
+  }
+  else {
+    if(numeroSecreto > chute) {
+      alert(`O número secreto é maior que ${chute}`);
+    }
+    else {
+      alert(`O número secreto é menor que ${chute}`);
+    }
+    tentativas++;
+  }
 }
-else {
-  alert(`Você errou :( O número secreto era ${numeroSecreto}.`);  
-}
+
