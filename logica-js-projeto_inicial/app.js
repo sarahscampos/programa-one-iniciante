d9dbd1609321983b89d6021fc67f055dc7515974
sarah-campos <sarahstephanycampos@gmail.com>
2024-06-22 19:14:27 -0300

    boas praticas

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,15 +1,15 @@
 alert('Boas-vindas ao jogo do número secreto!');
 
-let numeroSecreto = 29;
+let numeroSecreto = parseInt(Math.random() * 100 + 1); //parseInt pega a parte inteira do número
 console.log(numeroSecreto);
 let chute;
 let tentativas = 1;
 
 while(chute != numeroSecreto) {
-  chute = prompt('Escolha um número entre 1 e 30');
+  chute = prompt('Escolha um número entre 1 e 100');
 
   if(numeroSecreto == chute) {
-    alert(`Você acertou! O número secreto era ${numeroSecreto}! Você precisou de ${tentativas} tentativas.`)
+    break;
   }
   else {
     if(numeroSecreto > chute) {
@@ -21,4 +21,8 @@ while(chute != numeroSecreto) {
     tentativas++;
   }
 }
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Você acertou! O número secreto era ${numeroSecreto}! Você precisou de ${tentativas} ${palavraTentativa}.`)
+
 
