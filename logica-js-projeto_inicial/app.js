3ea6d2682efd0579539e22cab6b6dd1f7152f9f8
sarah-campos <sarahstephanycampos@gmail.com>
2024-06-22 19:23:31 -0300

    desafio

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,12 +1,13 @@
 alert('Boas-vindas ao jogo do número secreto!');
 
-let numeroSecreto = parseInt(Math.random() * 100 + 1); //parseInt pega a parte inteira do número
+let numeroMaximo = 1000;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1); //parseInt pega a parte inteira do número
 console.log(numeroSecreto);
 let chute;
 let tentativas = 1;
 
 while(chute != numeroSecreto) {
-  chute = prompt('Escolha um número entre 1 e 100');
+  chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
 
   if(numeroSecreto == chute) {
     break;
