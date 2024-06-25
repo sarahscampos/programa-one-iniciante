9985158fa0363b6514c0995e5bcabf98c76676d1
sarah-campos <sarahstephanycampos@gmail.com>
2024-06-24 22:15:01 -0300

    funcoes

--- js-curso-2/app.js
+++ js-curso-2/app.js
@@ -1,9 +1,27 @@
-let titulo = document.querySelector('h1');
-titulo.innerText = 'Jogo do número secreto';
+let numAleatorio = gerarNumeroAleatorio();
+console.log(numAleatorio);
 
-let paragrafo = document.querySelector('p');
-paragrafo.innerText = 'Escolha um número entre 1 e 10';
+exibirTexto('h1', 'Jogo do número secreto');
+exibirTexto('p', 'Escolha um número entre 1 e 10');
+
+function exibirTexto(seletor, texto) {
+  let campo = document.querySelector(seletor);
+  campo.innerText = texto;
+
+}
 
 function verificarChute() {
-  console.log('Clicou!');
+  let chute = document.querySelector('input').value;
+  if(chute == numAleatorio) {
+    console.log('Você acertou!!!');
+  }
+  else {
+    console.log('Você errou :( Tente de novo');
+  }
+
+}
+
+function gerarNumeroAleatorio() {
+  const numAleatorio = parseInt(Math.random() * 10 + 1);
+  return numAleatorio;
 }
